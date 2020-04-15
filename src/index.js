bcea4a2d08252201377389887e1c33c0f7d65a7b
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-04-15 23:06:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "routes";
+import TopBar from "components/topBar";
+import { CurrentUserProvider } from "contexts/currentUser";
+import CurrentUserChecker from 'components/currentUserChecker';
+
+const App = () => (
+  <CurrentUserProvider>
+    <CurrentUserChecker>
+      <Router>
+        <TopBar />
+        <Routes />
+      </Router>
+    </CurrentUserChecker>
+  </CurrentUserProvider>
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
