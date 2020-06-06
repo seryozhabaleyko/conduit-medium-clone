96ecfa0700d9bbb5ea3523011fd19be8c717f78b
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-06-06 13:15:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,21 +6,6 @@ import store from 'store';
 import App from 'components/App';
 import ErrorBoundary from 'components/ErrorBoundary';
 
-/* import Routes from "routes";
-import TopBar from "components/topBar";
-import { CurrentUserProvider } from "contexts/currentUser";
-import CurrentUserChecker from 'components/currentUserChecker';
-
-const App = () => (
-    <CurrentUserProvider>
-        <CurrentUserChecker>
-
-            <TopBar />
-            <Routes />
-        </CurrentUserChecker>
-    </CurrentUserProvider>
-); */
-
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
